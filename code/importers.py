572fcaeecce5a02d50cd00570b316e56981ca3a2
schlebusch <fabian.schlebusch@rwth-aachen.de>
2020-05-27 16:46:12 +0200

    Mechanical Stimuli are now also registered

--- code/importers.py
+++ code/importers.py
@@ -1,14 +1,16 @@
 import pandas as pd
 import numpy as np
-'''
+
+import signal_artifacts
 # reload the signal_artifacts classes
 # hacky workaround atm
 import importlib
 importlib.reload(signal_artifacts)
-'''
+
 # import our own classes for the signal artifacts
 from signal_artifacts import ActionPotential
 from signal_artifacts import ElectricalStimulus
+from signal_artifacts import MechanicalStimulus
 
 class SpikeImporter:
 	# the dataframe that we work with
@@ -86,14 +88,9 @@ class SpikeImporter:
 	
 		print("List of APs created.")
 		return actpots
-		
-		
-	# helper function to return rows that are unequal NaN
-	def getRowsWhereNotNaN(self, channel_name):
-		return self.df[~np.isnan(self.df[channel_name])]
-		
+				
 	# return the electrical pulses from the digmark channel
-	def getElectricalStimuli(self, channel_name):
+	def getElectricalStimuli(self):
 		# get rows where stimulus channel is one (where stimulus fired)
 		stimuli_df = self.getRowsWhereEqualsOne(self.stimulus_channel)
 		
@@ -106,6 +103,54 @@ class SpikeImporter:
 		print("List of eletrical stimuli created.")
 		return el_stimuli
 		
+	# return list of mechanical stimlui from the force channel
+	def getMechanicalStimuli(self, threshold, max_gap_time):
+		# get rows where force channel exceeds threshold
+		force_df = self.getRowsWhereExceedsThreshold(channel_name = self.force_channel, threshold = threshold)
+		
+		# then, create a list of the APs in this channel
+		mech_stimuli = []
+		index = 0
+		while index < len(force_df.index) - 1:
+			# assume that the first index is always the onset of an AP
+			onset = index
+			
+			len_df = len(force_df.index)
+			# increase the DF(!) index as long as the time distance to the next row is small enough
+			while (abs(force_df.iloc[index + 1][self.time_channel] - force_df.iloc[index][self.time_channel]) < max_gap_time):
+				index = index + 1
+				# break out of the loop if we reached the end
+				if (index == len_df - 1):
+					break
+				
+			# then, this is the last index
+			offset = index
+				
+			# create MS object from the shortened dataframe
+			# range does not include the last position, therefore + 1 !
+			# also, pass the electrical stimuli so that the class can get the closest one
+			# print(str(onset) + " to " + str(offset))
+			ms = MechanicalStimulus(input_df = force_df.iloc[range(onset, offset + 1)])
+			mech_stimuli.append(ms)
+			
+			# "jump" to the next AP
+			index = index + 1
+	
+		print("List of mechanical stimuli created.")
+		return mech_stimuli
+		
+		print(force_df)
+		
+		return
+		
 	# helper function that return rows where channel is one
 	def getRowsWhereEqualsOne(self, channel_name):
-		return self.df[self.df["32 DigMark"] == 1]
+		return self.df[self.df[channel_name] == 1]
+		
+	# helper function to return rows that are unequal NaN
+	def getRowsWhereNotNaN(self, channel_name):
+		return self.df[~np.isnan(self.df[channel_name])]
+		
+	# helper function to get rows that exceed a certain threshold
+	def getRowsWhereExceedsThreshold(self, channel_name, threshold):
+		return self.df[self.df[channel_name] > threshold]
--- code/signal_artifacts.py
+++ code/signal_artifacts.py
@@ -1,5 +1,38 @@
 import numpy as np
 
+'''
+	**********************************
+	A class for the mechanical stimuli
+	**********************************
+'''
+class MechanicalStimulus:
+	onset = None
+	offset = None
+	amplitude = None
+	
+	def __init__(self, input_df, time_column = "Time", force_column = "3 Force", verbose = False):
+		self.onset = input_df.iloc[0][time_column]
+		self.offset = input_df.iloc[-1][time_column]
+		
+		force_values = input_df[force_column].values
+		self.amplitude = max(force_values)
+		
+		if verbose == True:
+			print("Found mechanical stimulus:")
+			print("onset = " + str(self.onset) + "s offset = " + str(self.offset) + "s")
+			# print("values = " + str(force_values))
+			print("amplitude = " + str(self.amplitude) + "mN")
+			
+		return
+		
+	def getAmplitude(self):
+		return self.amplitude
+	
+'''
+	**********************************
+	A class for the APs
+	**********************************
+'''
 class ActionPotential:
 	onset = None
 	offset = None
@@ -64,6 +97,11 @@ class ActionPotential:
 	def getNormalizedEnergy(self):
 		return self.norm_energy
 		
+'''
+	**********************************
+	A class for the electrical stimuli
+	**********************************
+'''
 class ElectricalStimulus:
 	timepoint = None;
 	
