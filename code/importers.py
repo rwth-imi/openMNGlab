8606ec4684c16a47b96b8d81972faf1b2e65db7d
schlebusch <fabian.schlebusch@rwth-aachen.de>
2020-05-29 12:13:14 +0200

    The spike importer can now extract markers from different channels

--- code/importers.py
+++ code/importers.py
@@ -19,11 +19,11 @@ class SpikeImporter:
 	time_channel = None
 	signal_channel = None
 	stimulus_channel = None
-	ap_marker_channel = None
+	ap_marker_channels = None
 	force_channel = None
 	
 	# constructor loads a spike file from the given file path
-	def __init__(self, filepath, time_channel, signal_channel, stimulus_channel, ap_marker_channel = None, force_channel = None, custom_delimiter = ",", remove_apostrophes = True):
+	def __init__(self, filepath, time_channel, signal_channel, stimulus_channel, ap_marker_channels = [], force_channel = None, custom_delimiter = ",", remove_apostrophes = True):
 		# set up pandas to expect NaN values in the data and load the file 
 		self.df = pd.read_csv(filepath_or_buffer = filepath, delimiter = custom_delimiter, na_values = "NaN", na_filter = True)
 		
@@ -35,8 +35,8 @@ class SpikeImporter:
 		self.time_channel = time_channel
 		self.signal_channel = signal_channel
 		self.stimulus_channel = stimulus_channel
-		self.ap_marker_channel = ap_marker_channel
-		if ap_marker_channel == None:
+		self.ap_marker_channels = ap_marker_channels
+		if not ap_marker_channels:
 			print("No AP marker channel was given. But AP detection from signal is not yet implemented!")
 		self.force_channel = force_channel
 			
@@ -48,43 +48,46 @@ class SpikeImporter:
 	def getActionPotentials(self, max_gap_time, el_stimuli):
 		# catch some possible errors errors
 		# TODO: make sure that these errors cannot even occur
-		if self.ap_marker_channel == None:
+		if not self.ap_marker_channels:
 			print("Getting APs from the signal itself is not yet supported.")
 			return None
 		if el_stimuli == None:
 			print("Please give a list of the electrical stimuli in this measurement")
 			return None
 			
-		# get the rows from the AP where Spike registered some AP matching our template
-		actpots_df = self.getRowsWhereNotNaN(self.ap_marker_channel)
-		
-		# then, create a list of the APs in this channel
+		# get the APs from all marker channels
 		actpots = []
-		index = 0
-		while index < len(actpots_df.index) - 1:
-			# assume that the first index is always the onset of an AP
-			onset = index
+		for ap_marker_channel in self.ap_marker_channels:	
+			# get the rows from the AP where Spike registered some AP matching our template
+			actpots_df = self.getRowsWhereNotNaN(ap_marker_channel)
 			
-			len_df = len(actpots_df.index)
-			# increase the DF(!) index as long as the time distance to the next row is small enough
-			while (abs(actpots_df.iloc[index + 1][self.time_channel] - actpots_df.iloc[index][self.time_channel]) < max_gap_time):
+			# then, create a list of the APs in this channel
+			
+			index = 0
+			while index < len(actpots_df.index) - 1:
+				# assume that the first index is always the onset of an AP
+				onset = index
+				
+				len_df = len(actpots_df.index)
+				# increase the DF(!) index as long as the time distance to the next row is small enough
+				while (abs(actpots_df.iloc[index + 1][self.time_channel] - actpots_df.iloc[index][self.time_channel]) < max_gap_time):
+					index = index + 1
+					# break out of the loop if we reached the end
+					if (index == len_df - 1):
+						break
+					
+				# then, this is the last index
+				offset = index
+					
+				# create AP object from the shortened dataframe
+				# range does not include the last position, therefore + 1 !
+				# also, pass the electrical stimuli so that the class can get the closest one
+				# print(str(onset) + " to " + str(offset))
+				ap = ActionPotential(input_df = actpots_df.iloc[range(onset, offset + 1)], el_stimuli = el_stimuli)
+				actpots.append(ap)
+				
+				# "jump" to the next AP
 				index = index + 1
-				# break out of the loop if we reached the end
-				if (index == len_df - 1):
-					break
-				
-			# then, this is the last index
-			offset = index
-				
-			# create AP object from the shortened dataframe
-			# range does not include the last position, therefore + 1 !
-			# also, pass the electrical stimuli so that the class can get the closest one
-			# print(str(onset) + " to " + str(offset))
-			ap = ActionPotential(input_df = actpots_df.iloc[range(onset, offset + 1)], el_stimuli = el_stimuli)
-			actpots.append(ap)
-			
-			# "jump" to the next AP
-			index = index + 1
 	
 		print("List of APs created.")
 		return actpots
