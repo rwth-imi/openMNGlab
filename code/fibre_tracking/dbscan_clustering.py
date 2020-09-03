72f52ea6bef2cb21c68f5461dbbe0d79e74623d9
schlebusch <fabian.schlebusch@rwth-aachen.de>
2020-09-03 17:48:43 +0200

    Made DBSCAN clustering more interactive

--- code/fibre_tracking/dbscan_clustering.py
+++ code/fibre_tracking/dbscan_clustering.py
@@ -2,10 +2,13 @@ from metrics import clustering_metrics
 from sklearn.cluster import DBSCAN
 import numpy as np
 import pandas as pd
+from plotting import ClusterPlot2D
 
 class DBSCANClustering:
 
-	def perform_clustering(actpots, eps, min_samples, save_fibre_prediction = False) -> pd.DataFrame:
+	result_df = None
+
+	def perform_clustering(self, actpots, eps, min_samples, save_fibre_prediction = True, plot_results = False) -> pd.DataFrame:
 		# build feature vectors from the features provided by AP class
 		features = np.array([[ap.features["latency"], ap.features["energy"]] for ap in actpots])
 		channel_indices = np.array([ap.channel_index for ap in actpots])
@@ -17,10 +20,16 @@ class DBSCANClustering:
 			for ap, lbl in zip(actpots, labels):
 				ap.implied_fibre_index = lbl
 				
-		return pd.DataFrame(data = {'Onset': [ap.onset for ap in actpots],
+		self.result_df = pd.DataFrame(data = {'Onset': [ap.onset for ap in actpots],
 						'Offset': [ap.offset for ap in actpots],
 						'Latency': [ap.features["latency"] for ap in actpots],
 						'Energy': [ap.features["energy"] for ap in actpots],
 						'Channel_Index': channel_indices,
 						'Cluster_Index': labels}, 
 						columns = ['Onset', 'Offset', 'Latency', 'Energy', 'Channel_Index', 'Cluster_Index'])
+						
+		if plot_results == True:
+			cl2dplot = ClusterPlot2D()
+			cl2dplot.plot(clustered_data_df = self.result_df)
+						
+		return self.result_df
