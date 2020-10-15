2d74dfbc50db2f9c14a0919f6492eae82bf9a397
Fabian Schlebusch <fabian.schlebusch@rwth-aachen.de>
2020-10-15 17:06:14 +0200

    * Added AP tracks to falling leaf plot * Fixed attribute naming issue in ap track class

--- code/fibre_tracking/ap_track.py
+++ code/fibre_tracking/ap_track.py
@@ -6,14 +6,14 @@ class APTrack:
 
 	## this attribute stores the latencies at the individual sweeps
 	# they should be stored as tuples (k, t) where k is the sweep index and t is the latency at the sweep k
-	latencies = None
+	_latencies = None
 	
 	## Construct an object for an AP track in the recording
 	# @param latencies A list of tuples (sweep_idx, latency) where sweep_idx is the index of the sweep (also called k in the paper) and the latency t (in seconds)
 	def __init__(self, latencies):
 		
 		# store the latency tuples in a sorted list
-		self.latencies = sorted(latencies, key = lambda latency: latency[0])
+		self._latencies = sorted(latencies, key = lambda latency: latency[0])
 	
 	def extend_upwards(self):
 		pass
@@ -22,11 +22,11 @@ class APTrack:
 	def extend_downwards(self, sweeps, max_shift = 0.01, radius = 2, window_size = 0.0015):
 		
 		# Get the last R (radius) latencies to fit a line
-		sweep_idcs = [x[0] for x in self.latencies[-radius - 1 : ]]
-		latencies = [x[1] for x in self.latencies[-radius - 1 : ]]
+		sweep_idcs = self.sweep_idcs
+		latencies = self.latencies
 	
 		# fit a line to the existing sweep indices and latencies to get estimates for the line parameters
-		if len(self.latencies) >= radius:
+		if len(self._latencies) >= radius:
 			slope, latency_intercept = np.polyfit(x = sweep_idcs, y = latencies, deg = 1)
 		
 		# using these parameters, predict the latency in the very next sweep
@@ -45,7 +45,15 @@ class APTrack:
 		
 		# go through the existing latencies to insert the new one in the right place
 		lst_idx = 0
-		while lst_idx < len(self.latencies) and sweep_idx > self.latencies[lst_idx][0]:
+		while lst_idx < len(self._latencies) and sweep_idx > self._latencies[lst_idx][0]:
 			lst_idx += 1
 			
-		self.latencies.insert(lst_idx, (sweep_idx, latency))
+		self._latencies.insert(lst_idx, (sweep_idx, latency))
+		
+	@property
+	def sweep_idcs(self):
+		return [x[0] for x in self._latencies]
+		
+	@property
+	def latencies(self):
+		return [x[1] for x in self._latencies] 
