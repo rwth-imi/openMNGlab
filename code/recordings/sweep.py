e44cc38cb2c27a5cf59f8e780f1f3d0259516267
Fabian Schlebusch <fabian.schlebusch@rwth-aachen.de>
2020-11-04 19:17:54 +0100

    Added comments to the sweep class

--- code/recordings/sweep.py
+++ code/recordings/sweep.py
@@ -5,15 +5,23 @@ from typing import Dict, List
 # A sweep is the time interval between two electrical stimuli.
 class Sweep:
 	
-	# TODO: Add comments
-	
+	## Electrical stimulus in the beginning of this sweep
 	el_stimulus = None
+	## Raw signal during this sweep
 	raw_signal = None
+	## List of APs in this sweep
 	action_potentials = None
-	
+	## Starting time
 	t_start = None
+	## End time
 	t_end = None
 	
+	## Constructs a new sweep object
+	# @param stimulus Electrical stimulus in the beginning of this sweep
+	# @param actpots Action potentials that follow this stimulus
+	# @param raw_signal Raw signal for this sweep
+	# @param t_start Start time of the sweep
+	# @param t_end Analogous: end time
 	def __init__(self, stimulus, actpots, raw_signal, t_start, t_end):
 		self.el_stimulus = stimulus
 		self.action_potentials = actpots
